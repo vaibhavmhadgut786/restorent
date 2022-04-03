7dd41a864edffc44208c05fd445f8f1efb7c3456
vaibhavmhadgut786 <vaibhavmhadgut786@gmail.com>
2022-04-03 16:02:37 +0530

    fix:

--- src/components/MenuCard.js
+++ src/components/MenuCard.js
@@ -15,8 +15,8 @@ const MenuCard = ({ menuData }) => {
                   <h1 className="card-title">{name}</h1>
                   <span className="card-description subtle">{description}</span>
                   <div className="card-read">read</div>
+                  <img src={image} alt="images" className="card-media" />
                 </div>
-                <img src={image} alt="images" className="card-media" />
                 <span className="card-tag  subtle">Order Now</span>
               </div>
             </div>
